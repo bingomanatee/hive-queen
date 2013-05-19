c54b3c7fa84b228d79dfe728e69d56352bb565f9
pc-dedelhart <david.edelhart@personalcapital.com>
2013-05-19 10:22:45 -0700

    reorganizing test output directories; attempting to clean up code for handlers

--- tests/test_parsers.js
+++ tests/test_parsers.js
@@ -2,7 +2,9 @@ var tap = require('tap');
 var path = require('path');
 var util = require('util');
 var _ = require('underscore');
-var queen = require('./../lib/index')
+var queen = require('./../lib/index');
+var ndd = require('node-dir-diff');
+var rmdir = require('rmdir');
 
 var _DEBUG = false;
 
@@ -15,12 +17,31 @@ var root = path.resolve(__dirname, '../test_resources');
 if (true) {
 	tap.test('parser scanning', function (t) {
 		var scan_def = require(path.resolve(root, 'configs/spawn/spawn_2.json'));
-		queen.frames_parser(path.resolve(root, 'spawn_site_2'))
-			.then(function (parser) {
-				parser.parse(scan_def, function () {
-					t.end();
+		var write_dir = path.resolve(root, 'output/spawn_site_2');
+		var exp_dir = path.resolve(root, 'expectations/spawn_site_2');
+		rmdir(write_dir, function () {
+
+			queen.frames_parser(write_dir)
+				.then(function (parser) {
+					parser.parse(scan_def, function () {
+
+						var dd = new ndd.Dir_Diff(
+							[
+								exp_dir,
+								write_dir
+							],
+							'full'
+						);
+
+						dd.compare(function (err, result) {
+							t.equal(result.deviation, 0, 'no deviations in expectations');
+							rmdir(write_dir, function(){
+								t.end();
+							})
+						});
+					});
 				});
-			});
+		});
 	});// end tap.test 1
 }
 
